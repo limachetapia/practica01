1c72ac1fdc8e635044c5a5edf2df0c61c479abe1
Patron048 <Patron048@Limache048>
2018-10-10 08:44:09 -0500

    modificando el codigo

--- AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
+++ AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -11,7 +11,7 @@ public class AplicacionPrueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("HOLA MUNDO");
         
     }
     
