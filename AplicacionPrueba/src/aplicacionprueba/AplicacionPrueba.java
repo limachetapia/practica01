b7f71fd3a97a4048311c074094a4dfff0b80d76f
Patron048 <Patron048@Limache048>
2018-10-10 08:40:34 -0500

    subiendo el primer commit

---
+++ AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -0,0 +1,18 @@
+
+package aplicacionprueba;
+
+/**
+ *
+ * @author Patron048
+ */
+public class AplicacionPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
